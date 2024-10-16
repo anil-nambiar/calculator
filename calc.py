76d92cd393b5e605e3378bb82e6d04040b08a974
anil-nambiar <96141521+anil-nambiar@users.noreply.github.com>
2024-10-16 18:01:50 +0530

    Update calc.py
    Added feature to multiply

--- calc.py
+++ calc.py
@@ -2,11 +2,13 @@ x = int(input("Enter first integer: "))
 y = int(input("Enter second integer: "))
 
 while True:
-    n = int(input("Enter action:\n1. Add\n2. Subtract\n3. Exit\n"))
+    n = int(input("Enter action:\n1. Add\n2. Subtract\n3. Multiply\n4. Exit\n"))
     if n==1:
         print("Sum is", x+y)
     elif n==2:
         print("Difference is", x-y)
     elif n==3:
+        print("Product is", x*y)
+    elif n==4:
         print("Goodbye!")
         break
